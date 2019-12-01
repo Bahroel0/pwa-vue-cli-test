aed29157971bb07956a57749460f0c4344efed73
Bahroel0 <bahrul123amaruddin@gmail.com>
2019-12-01 07:35:15 +0700

    delete dist

--- src/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.supperssWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-self.addEventListener("message", event => {
-  if (event.data && event.data.action === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  pwa: {
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      swSrc: "src/service-worker.js"
-    }
-  }
+  transpileDependencies: ["vuetify"]
 };
